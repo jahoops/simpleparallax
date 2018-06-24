56e364b8b2f5422d641837a64607a2a79e722dbe
jahoops <jwhooper@gmail.com>
2018-06-24 17:07:10 -0400

    up

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
-function scrollSlide(el,targetid) {
-  document.querySelectorAll('#nav a.active').forEach( 
-    function(el){el.classList.remove('active');} 
+function scrollSlide(el, targetid) {
+  document.querySelectorAll('#nav a.active').forEach(
+    function (el) {
+      el.classList.remove('active');
+    }
   );
   el.classList.add('active');
   var targetDiv = document.querySelector(targetid);
@@ -10,4 +12,19 @@ function scrollSlide(el,targetid) {
     behavior: 'smooth'
   });
 }
+
+$( document ).ready(function() {
+
+  var rellax = new Rellax('.rellax');
+
+  $('.clockprojecticon').on('mouseenter', function(){
+    if($('#clock').css('top')!=='3vh') {
+      $('#clock').css('top','3vh');
+    }
+  });
+  $('.clockprojecticon').on('mouseleave', function(){
+    $('#clock').css('top','250vh');
+  });
+
+});
 
