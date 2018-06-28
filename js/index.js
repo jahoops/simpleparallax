7082617eb20a3c84eca9f92fc39ba1eb7d6394e6
jahoops <jwhooper@gmail.com>
2018-06-27 21:58:37 -0400

    up

--- js/index.js
+++ js/index.js
@@ -13,19 +13,79 @@ function scrollSlide(el, targetid) {
   });
 }
 
-$( document ).ready(function() {
+$(document).ready(function () {
 
   var rellax = new Rellax('.rellax');
 
-  $('.clockprojecticon').on('mouseenter', function(){
-    if($('#clock').css('top')!=='3vh') {
-      $('#clock').css('top','3vh');
+  $('.clockprojecticon').on('mouseenter', function () {
+    if ($('#clock').css('top') !== '3vh') {
+      $('#clock').css('top', '3vh');
     }
   });
-  $('.clockprojecticon').on('mouseleave', function(){
-    $('#clock').css('top','250vh');
+  $('.clockprojecticon').on('mouseleave', function () {
+    $('#clock').css('top', '250vh');
   });
-  
-  renderLogo();
 
 });
+
+~ function () {
+
+  'use strict';
+
+  function run() {
+
+    requestAnimationFrame(run);
+    r = ((screen.offsetWidth / 2) - pointer.ex) / 2;
+    pointer.ease(15);
+    live.style[transform] = 'rotateY(' + r + 'deg)';
+
+    for (var i = 0; i < numLetters; i++) {
+      letters[i].style[transform] = 'rotateY(' + (-r) + 'deg)';
+      var c = (70 - (i - 1.5) * Math.sin(r * Math.PI / 180) * 40);
+      letters[i].style.color = 'hsl(0, 0%, ' + c + '%)';
+    }
+  }
+
+  var live = document.getElementById('live'),
+    transform = getPrefix(['transform', 'WebkitTransform']),
+    letters = live.children,
+    numLetters = letters.length,
+    r = 0;
+
+  function getPrefix(prefixes) {
+    for (var test, i = 0; test = prefixes[i++];) {
+      if (typeof document.body.style[test] != 'undefined') return test;
+    }
+    return null;
+  }
+
+  var screen = document.getElementById('screen');
+  var pointer = (function (screen) {
+    var pointer = {
+      x: screen.offsetWidth / 2,
+      ex: screen.offsetWidth / 2 - 180,
+      pointer: function (e) {
+        var touchMode = e.targetTouches,
+          pointer;
+        if (touchMode) {
+          e.preventDefault();
+          pointer = touchMode[0];
+        } else pointer = e;
+        this.x = pointer.clientX;
+      },
+      ease: function (steps) {
+        this.ex += (this.x - this.ex) / steps;
+      }
+    };
+    window.addEventListener('mousemove', function (e) {
+      this.pointer(e);
+    }.bind(pointer), false);
+    window.addEventListener('touchmove', function (e) {
+      this.pointer(e);
+    }.bind(pointer), false);
+    return pointer;
+  }(screen));
+
+  run();
+
+}();
